5cd283c0d36fe15aa57cec810a59589cceb9d266
justhoop <justin@fishzen.com>
2020-12-22 20:30:28 -0500

    added regex, different answer, still wrong

--- day4/day4.py
+++ day4/day4.py
@@ -1,67 +1,55 @@
+import re
+
 def parttwo(passport):
-    good = 0
+    bad = 0
     failed = ""
-    if not daterange(passport["byr"], 1920, 2002): #byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    if not int(passport["byr"]) >= 1920 and int(passport["byr"]) <= 2002: #byr (Birth Year) - four digits; at least 1920 and at most 2002.
         failed = failed + "byr "
-        good += 1
-    if not daterange(passport["iyr"], 2010, 2020): #iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+        bad += 1
+    if not int(passport["iyr"]) >= 2010 and int(passport["iyr"]) <= 2020: #iyr (Issue Year) - four digits; at least 2010 and at most 2020.
         failed = failed + "iry "
-        good += 1
-    if not daterange(passport["eyr"], 2020, 2030): #eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+        bad += 1
+    if not int(passport["eyr"]) >= 2020 and int(passport["eyr"]) <= 2030: #eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
         failed = failed + "eyr "
-        good += 1
+        bad += 1
     if not height(passport["hgt"]): #hgt (Height) - a number followed by either cm or in:
         failed = failed + "hgt "
-        good += 1
+        bad += 1
     if not hair(passport["hcl"]):
         failed = failed + "hcl "
-        good += 1
+        bad += 1
     if not passport["ecl"] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']: #ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
         failed = failed + "ecl "
-        good += 1
+        bad += 1
     if not len(passport["pid"]) == 9: #pid (Passport ID) - a nine-digit number, including leading zeroes.
         failed = failed + "pid "
-        good += 1
+        bad += 1
     
-    if good > 0:
+    if bad > 0:
         return False
     else:
         return True
 
 def hair(color):
-    if '#' in color[0]: #hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-        color = color.removeprefix('#')
-        if len(color) == 6:
-            let = ['a', 'b', 'c', 'd', 'e', 'f']
-            num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-            for char in color:
-                if not char in let and not char in num:
-                    return False
-            return True
-        else:
-            return False
+    pattern = re.compile(r'^(#)([a-fA-F0-9]){6}')
+    if pattern.fullmatch(color):
+        return True
     else:
         return False
 
 def height(height):
     if 'in' in height:
         height = height.removesuffix('in')
-        if int(height) > 58 and int(height) < 77: #If in, the number must be at least 59 and at most 76.
+        if int(height) >= 59 and int(height) <= 76: #If in, the number must be at least 59 and at most 76.
             return True
         else:
             return False
-    if 'cm' in height:
+    elif 'cm' in height:
         height = height.removesuffix('cm')
-        if int(height) > 149 and int(height) < 194: #If cm, the number must be at least 150 and at most 193.
+        if int(height) >= 150 and int(height) <= 193: #If cm, the number must be at least 150 and at most 193.
             return True
         else:
             return False
-
-def daterange(year, low, high):
-    if int(year) >= low and int(year) <= high:
-        return True
-    else:
-        return False
 
 def firstanswer(passports):
     fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
@@ -98,6 +86,7 @@ with open("day4/day4.txt", "r") as f:
         else:
             passports.append(passport)
             passport = []
+    passports.append(passport)
     print('end')
 
 print(firstanswer(passports))
