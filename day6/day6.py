af524a779da7e94a93da47ade79c1f6a09cb1c89
justhoop <justin@fishzen.com>
2020-12-24 16:37:50 -0500

    likes python standard library

--- day6/day6.py
+++ day6/day6.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 answers = []
 group = []
 with open("day6/day6.txt", "r") as f:
@@ -15,4 +17,27 @@ with open("day6/day6.txt", "r") as f:
 total = 0
 for i in answers:
     total = total + len(i)
+print(total)
+
+total = 0
+answers = []
+group = []
+with open("day6/day6.txt", "r") as f:
+    for line in f:
+        if line != '\n':
+            entry = line.replace('\n', '')
+            group.append(entry)
+        else:
+            answers.append(group)
+            group = []
+    answers.append(group)
+for group in answers:
+    x = ""
+    members = len(group)
+    for answer in group:
+        x += answer
+    c = Counter(x)
+    for e in c.values():
+        if e == members:
+            total += 1
 print(total)
